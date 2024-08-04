aa979ccc735fae75f2517bf34fc903c66a6fd455
Bilan Vadim <vadimb892@gmail.com>
2024-08-04 14:40:55 +0300

    Added files and modules

--- crates/bin/spaceship_game/src/main.rs
+++ crates/bin/spaceship_game/src/main.rs
@@ -1,3 +1,17 @@
+mod schedule;
+mod asset_loader;
+mod camera;
+mod asteroids;
+mod spaceship;
+mod movement;
+mod collision_detection;
+mod despawn;
+mod health;
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .run();
 }
